1cd4448a59fba98c4bf5b75c00df3cf399d70555
PetrDidur <pdidur@yandex.ru>
2023-04-18 14:14:28 +0300

    homework 1st part

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_item_class():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1.name == "Смартфон"
+    assert item1.price == 10_000
+    assert item1.quantity == 20
+
+    assert item1.calculate_total_price() == "Total price = 200000"
+
+    item1.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 5_000
+
+    assert item2.name == "Ноутбук"
+    assert item2.price == 20_000
+    assert item2.quantity == 5
+
+    assert item2.calculate_total_price() == "Total price = 100000"
